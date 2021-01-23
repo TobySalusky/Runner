e5a2f2b41ad250769cd1247bd1bc6c9a559b7be0
Longineese <50775507+Longineese@users.noreply.github.com>
2021-01-23 15:26:35 -0500

    Improved Jumping

--- src/Entity/Player.cs
+++ src/Entity/Player.cs
@@ -4,19 +4,23 @@ using Microsoft.Xna.Framework.Input;
 namespace Runner {
     public class Player : Entity {
 
-        public float speed = 30, jumpHeight = 2;
+        public float speed = 30, jumpHeight = 5;
+        public float jumpTime;
+        
+        public const float jumpTimeStart = 0.6F;
         
         
-        public Player(Vector2 pos) : base(pos, -1) {
-            
-        }
+        public Player(Vector2 pos) : base(pos, -1) { }
 
         public override void update(float deltaTime) {
             base.update(deltaTime);
+            
+            
         }
         
         public virtual void jump(float jumpHeight) {
             vel.Y -= Util.heightToJumpPower(jumpHeight, gravity);
+            jumpTime = jumpTimeStart;
         }
 
         public void input(KeyInfo keys, float deltaTime) {
@@ -30,8 +34,17 @@ namespace Runner {
             float accelSpeed = (inputX == 0 && grounded) ? 5 : 2.5F;
             vel.X += ((inputX * speed) - vel.X) * deltaTime * accelSpeed;
 
-            if (grounded && keys.down(Keys.Space)) {
+
+            jumpTime -= deltaTime;
+            
+            if (grounded && keys.down(Keys.Space) && jumpTime < jumpTimeStart - 0.1F) {
+
                 jump(jumpHeight);
+            }
+
+            if (!grounded && keys.down(Keys.Space) && jumpTime > 0) {
+                float fade = jumpTime / jumpTimeStart;
+                vel.Y -= 50F * deltaTime * fade;
             }
         }
     }
